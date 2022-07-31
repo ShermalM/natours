741c60809c5e85e7206f36468a79e67a0c8cf232
Shermal Marasinghe <shermalmarasinghe@gmail.com>
2022-07-31 16:34:11 -0400

    Fixed issue with bundled index.js 2

--- public/js/bundled/index.js
+++ public/js/bundled/index.js
@@ -531,7 +531,6 @@ function hmrAcceptRun(bundle, id) {
 }
 
 },{}],"8tp2c":[function(require,module,exports) {
-var _polyfill = require("@babel/polyfill");
 var _mapbox = require("./mapbox");
 var _login = require("./login");
 var _updateSettings = require("./updateSettings");
--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,5 @@
-import '@babel/polyfill';
+//import '@babel/polyfill';
+import "core-js/stable";
 import {displayMap} from './mapbox';
 import {login, logout} from './login';
 import {updateSettings} from './updateSettings';
