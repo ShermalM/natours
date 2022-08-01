81409c439208d9579eaa8740df98d0d0b4a70d1a
Shermal Marasinghe <shermalmarasinghe@gmail.com>
2022-08-01 16:48:31 -0400

    Modified updateSettings to automatically reload the page once the user saves settings

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -17,6 +17,9 @@ export const updateSettings = async (data, type) => {
 
         if(result.data.status === 'success'){
             showAlert('success',`${type.toUpperCase()} updated successfully!`);
+            window.setTimeout(()=> {
+                location.reload(true);
+            }, 1500);
         } 
     } catch(err){
         showAlert('error', err.response.data.message);
